f5e5e773f5c4bf06bb792c357c562f659fb1b880
unknown <aditya_hermansyahputra@yahoo.com>
2019-10-07 18:37:51 +0700

    base

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,9 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -13,12 +12,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
   bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
-        .copyWith(statusBarColor: Colors.transparent));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent));
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -27,70 +26,49 @@ class _LoginPageState extends State<LoginPage> {
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter),
         ),
-        child: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView(
-                children: <Widget>[
-                  headerSection(),
-                  textSection(),
-                  bottonSection()
-                ],
-              ),
+        child: _isLoading ? Center(child: CircularProgressIndicator()) : ListView(
+          children: <Widget>[
+            headerSection(),
+            textSection(),
+            buttonSection(),
+          ],
+        ),
       ),
     );
   }
 
-  Container headerSection() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      child: Text('Code Land', style: TextStyle(color: Colors.white)),
-    );
-  }
-
-  Container textSection() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      margin: EdgeInsets.only(top: 30),
-      child: Column(
-        children: <Widget>[
-          tvEmail("Email", Icons.email),
-          SizedBox(height: 30),
-          tvPassword("Password", Icons.lock)
-        ],
-      ),
-    );
-  }
-
-  signIn(String email, password) async {
+  signIn(String email, pass) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    Map data = {'email': email, 'password': password};
+    Map data = {
+      'email': email,
+      'password': pass
+    };
     var jsonResponse = null;
-    var response = await http.post("BASE_URL", body: data);
-    if (response.statusCode == 200) {
+    var response = await http.post("http://comic.id/api-ecourse/user/login.php", body: data);
+    if(response.statusCode == 200) {
       jsonResponse = json.decode(response.body);
-      if (jsonResponse != null) {
+      if(jsonResponse != null) {
         setState(() {
           _isLoading = false;
         });
         sharedPreferences.setString("token", jsonResponse['token']);
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (BuildContext context) => MainPage()),
-            (Route<dynamic> route) => false);
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => MainPage()), (Route<dynamic> route) => false);
       }
-    } else {
+    }
+    else {
       setState(() {
         _isLoading = false;
       });
-      print(response.body)
+      print(response.body);
     }
   }
 
-  Container bottonSection() {
+  Container buttonSection() {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: 40,
-      margin: EdgeInsets.only(top: 15),
-      padding: EdgeInsets.symmetric(horizontal: 15),
+      height: 40.0,
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      margin: EdgeInsets.only(top: 15.0),
       child: RaisedButton(
         onPressed: emailController.text == "" || passwordController.text == "" ? null : () {
           setState(() {
@@ -98,10 +76,10 @@ class _LoginPageState extends State<LoginPage> {
           });
           signIn(emailController.text, passwordController.text);
         },
-        elevation: 0,
+        elevation: 0.0,
         color: Colors.purple,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
         child: Text("Sign In", style: TextStyle(color: Colors.white70)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
       ),
     );
   }
@@ -109,26 +87,50 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
 
-  TextFormField tvEmail(String title, iconData, icon) {
-    return TextFormField(
-      controller: emailController,
-      style: TextStyle(color: Colors.white70),
-      decoration: InputDecoration(
-          hintText: title,
-          hintStyle: TextStyle(color: Colors.white70),
-          icon: Icon(icon)),
+  Container textSection() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: emailController,
+            cursorColor: Colors.white,
+
+            style: TextStyle(color: Colors.white70),
+            decoration: InputDecoration(
+              icon: Icon(Icons.email, color: Colors.white70),
+              hintText: "Email",
+              border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+              hintStyle: TextStyle(color: Colors.white70),
+            ),
+          ),
+          SizedBox(height: 30.0),
+          TextFormField(
+            controller: passwordController,
+            cursorColor: Colors.white,
+            obscureText: true,
+            style: TextStyle(color: Colors.white70),
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock, color: Colors.white70),
+              hintText: "Password",
+              border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+              hintStyle: TextStyle(color: Colors.white70),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  TextFormField tvPassword(String title, iconData, icon) {
-    return TextFormField(
-      controller: passwordController,
-      obscureText: true,
-      style: TextStyle(color: Colors.white70),
-      decoration: InputDecoration(
-          hintText: title,
-          hintStyle: TextStyle(color: Colors.white70),
-          icon: Icon(icon)),
+  Container headerSection() {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+      child: Text("Code Land",
+          style: TextStyle(
+              color: Colors.white70,
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold)),
     );
   }
 }
