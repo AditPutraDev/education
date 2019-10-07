fe666273df1864d728132b242286c686ed755d40
unknown <aditya_hermansyahputra@yahoo.com>
2019-10-07 19:02:52 +0700

    add nav to reg

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:education/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
@@ -12,12 +13,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-  bool _isLoading = false;
+  bool onLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+        .copyWith(statusBarColor: Colors.transparent));
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -26,38 +27,39 @@ class _LoginPageState extends State<LoginPage> {
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter),
         ),
-        child: _isLoading ? Center(child: CircularProgressIndicator()) : ListView(
-          children: <Widget>[
-            headerSection(),
-            textSection(),
-            buttonSection(),
-          ],
-        ),
+        child: onLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView(
+                children: <Widget>[
+                  headerSection(),
+                  textSection(),
+                  buttonSection(),
+                ],
+              ),
       ),
     );
   }
 
   signIn(String email, pass) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    Map data = {
-      'email': email,
-      'password': pass
-    };
+    Map data = {'email': email, 'password': pass};
     var jsonResponse = null;
-    var response = await http.post("http://comic.id/api-ecourse/user/login.php", body: data);
-    if(response.statusCode == 200) {
+    var response = await http.post("http://comic.id/api-ecourse/user/login.php",
+        body: data);
+    if (response.statusCode == 200) {
       jsonResponse = json.decode(response.body);
-      if(jsonResponse != null) {
+      if (jsonResponse != null) {
         setState(() {
-          _isLoading = false;
+          onLoading = false;
         });
         sharedPreferences.setString("token", jsonResponse['token']);
-        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => MainPage()), (Route<dynamic> route) => false);
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (BuildContext context) => MainPage()),
+            (Route<dynamic> route) => false);
       }
-    }
-    else {
+    } else {
       setState(() {
-        _isLoading = false;
+        onLoading = false;
       });
       print(response.body);
     }
@@ -65,21 +67,48 @@ class _LoginPageState extends State<LoginPage> {
 
   Container buttonSection() {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 40.0,
-      padding: EdgeInsets.symmetric(horizontal: 15.0),
-      margin: EdgeInsets.only(top: 15.0),
-      child: RaisedButton(
-        onPressed: emailController.text == "" || passwordController.text == "" ? null : () {
-          setState(() {
-            _isLoading = true;
-          });
-          signIn(emailController.text, passwordController.text);
-        },
-        elevation: 0.0,
-        color: Colors.purple,
-        child: Text("Sign In", style: TextStyle(color: Colors.white70)),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      child: Column(
+        children: <Widget>[
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              margin: EdgeInsets.only(top: 15),
+              child: RaisedButton(
+                onPressed: emailController.text == "" ||
+                        passwordController.text == ""
+                    ? null
+                    : () {
+                        setState(() {
+                          onLoading = true;
+                        });
+                        signIn(emailController.text, passwordController.text);
+                      },
+                elevation: 0.0,
+                color: Colors.blue,
+                child: Text("Sign In", style: TextStyle(color: Colors.white70)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+              )),
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              margin: EdgeInsets.only(top: 15),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegisterPage()));
+                },
+                elevation: 0.0,
+                color: Colors.blue,
+                child:
+                    Text("Register", style: TextStyle(color: Colors.white70)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+              )),
+        ],
       ),
     );
   }
@@ -89,18 +118,18 @@ class _LoginPageState extends State<LoginPage> {
 
   Container textSection() {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
       child: Column(
         children: <Widget>[
           TextFormField(
             controller: emailController,
             cursorColor: Colors.white,
-
             style: TextStyle(color: Colors.white70),
             decoration: InputDecoration(
               icon: Icon(Icons.email, color: Colors.white70),
               hintText: "Email",
-              border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white70)),
               hintStyle: TextStyle(color: Colors.white70),
             ),
           ),
@@ -113,7 +142,8 @@ class _LoginPageState extends State<LoginPage> {
             decoration: InputDecoration(
               icon: Icon(Icons.lock, color: Colors.white70),
               hintText: "Password",
-              border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white70)),
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white70)),
               hintStyle: TextStyle(color: Colors.white70),
             ),
           ),
@@ -124,8 +154,8 @@ class _LoginPageState extends State<LoginPage> {
 
   Container headerSection() {
     return Container(
-      margin: EdgeInsets.only(top: 50.0),
-      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+      margin: EdgeInsets.only(top: 50),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
       child: Text("Education",
           style: TextStyle(
               color: Colors.white70,
